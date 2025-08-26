831975429433aa5902bb02665b1e91c71ac77711
Physix <lolathecat16@outlook.com>
2025-08-26 11:42:26 -0600

    insaulting Apple and MacOS

--- CalderaEngine.cpp
+++ CalderaEngine.cpp
@@ -18,7 +18,8 @@ int main() {
     // 1. Initialize GLFW
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2); // macOS prefers 3.2
+    // MacOS for some reason likes 3.2 but not other versions
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Required on macOS
